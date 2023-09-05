ae05e52b204709a8133a5bba7530feba5ba22adc
GabrielLoboSilva <nunesgabriel007@gmail.com>
2023-09-05 12:51:13 -0300

    Adcionei uma imagem ao fundo do site

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const icon = document.getElementById('icon');
 function showHide(){
     if(password.type === 'password'){
         password.setAttribute('type', 'text');
-        icon.classList.remove('hide')
+        icon.classList.add('hide')
     }else{
         password.setAttribute('type', 'password');
-        icon.classList.add('hide')
+        icon.classList.remove('hide')
     }
 }
